6c3c3f31967bfa916ba5ee79da5265ed336f4a3d
samuel <samusplay08@gmail.com>
2026-04-23 12:41:30 -0500

    cambio el cuenta schema

--- app/schemas/cuenta.schema.ts
+++ app/schemas/cuenta.schema.ts
@@ -3,9 +3,16 @@ import { z } from "zod";
 
 const TIPOS_CUENTA = ["ahorros", "corriente"] as const;
 
+const soloLetrasRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+
 export const createCuentaSchema = z.object({
   numero_cuenta: z.number().min(1, "Número de cuenta requerido"),
-  nombre_cliente: z.string().min(3, "Mínimo 3 caracteres"),
+  nombre_cliente: z
+  .string()
+  .min(3, "Mínimo 3 caracteres")
+  .regex(soloLetrasRegex,{
+    message: "El nombre solo puede contener letras y espacios"
+  }),
   saldo: z.number().min(0, "El saldo no puede ser negativo"),
   tipo_cuenta: z.enum(TIPOS_CUENTA),
 });
